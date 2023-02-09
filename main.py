8e6497c6e9a7fb902603786a77890038b4183858
zeinn <good_zein@mail.ru>
2023-02-09 18:48:29 +0300

    next try

--- main.py
+++ main.py
@@ -28,10 +28,18 @@ school = {'1a': 25, '2c': 18, '3b': 20, '4c': 28, '5a': 31, '6b': 16, '7a': 30, 
           '10b': 26}  # Задание 6
 print(school.get("5a"))
 
+l_1 =  [1,2,3,4,5] # Задание 7
+print(l_1 [1]) # вывод 2 символа
+
 str_2 = 'employment' # Задание 8
 str_3 = str_2.find('employ')
 print(str_3)
 
-x = 'My name is Agent Smith'
-x_1 = [1]
-print(x_1)
+x = 'My name is Agent Smith' # Задание 9
+print(x [1]) # выводит символ Y
+print(x [3], x[6], x[9], x[12], x[15])
+
+l_2 = [1,5,2,9,2,9,1]
+uniq_num = list(set(l_2))
+print(uniq_num)
+
