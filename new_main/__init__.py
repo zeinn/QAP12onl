cdae5e4d067549ba1441a239bbbcce9920c196b7
zeinn <good_zein@mail.ru>
2023-02-19 16:44:26 +0300

    replacing multiple answers on functions. create __init__.py

---
+++ new_main/__init__.py
@@ -0,0 +1,1 @@
+from new_main.main import *
--- new_main/main.py
+++ new_main/main.py
@@ -6,12 +6,11 @@ v_4 = int(v_2)
 print('число, приведенное к целому типу:', v_3)
 print('число, приведенное к целому типу:', v_4)
 
-str_old = 'www.my_site.com#about'
-str_new = 'www.my_site.com#about'  # Задание 2
-str_new = str_new.replace('#', '/')
-
-print('Перноначальная строка:', str_old)
-print('Ррезьтутат с заменой символа:', str_new)
+def change(a, s_1, s_2):
+    if type(a) == str:
+        return a.replace(s_1, s_2)
+    else:
+        return a
 
 str_new = 'stroka'  # Задание 3
 str_b = str_new.replace('a', 'ing')
@@ -23,8 +22,13 @@ str_i = str_ivanou.split(" ")[1] + ' ' + str_ivanou.split(" ")[0]
 print('Строка из условия:', str_ivanou)
 print('Строка после замены слов:', str_i)
 
-s = input('Введите с помощью клавиатуры слово с изначальным и конечным пробелом:').strip()  # Задание 5
-print('Пробелы удалены:', s)
+
+def del_sp(str_1):
+    if type(str_1) == str:
+        return str_1.strip()
+
+
+
 
 school = {'1a': 25, '2c': 18, '3b': 20, '4c': 28, '5a': 31, '6b': 16, '7a': 30, '8d': 15, '9a': 22,
           '10b': 26}  # Задание 6
@@ -33,14 +37,15 @@ print('Вывели количество учеников по ключу 5а', 
 l_1 = [1, 2, 3, 4, 5]  # Задание 7
 print('Второй символ выведен:', l_1[1])  # вывод 2 символа
 
-str_2 = 'employment'  # Задание 8
-str_3 = str_2.find('employ')
-print('Строка str_3 входит в строку str_2:', str_3)
+def string(str_1, str_2):
+    if type(str_1) and type(str_2) == str:
+        return str_1 in str_2
+    else:
+        return str_1, str_2
+
 
 x = 'My name is Agent Smith'  # Задание 9
 print('Вывели символ по индексу [1]:', x[1])  # выводит символ Y
 print('Вывели символы по индексу [3]:, x[6], x[9], x[12], x[15] ', x[3], x[6], x[9], x[12], x[15])
 
-l_2 = [1, 5, 2, 9, 2, 9, 1]  # Задание 10
-uniq_num = list(set(l_2))
-print('Уникальные числа:', uniq_num)
+
